c3ba72472d8f5abb698be8d20e747c9e3dc047d1
Mstislav <mstislavd@gmail.com>
2024-03-25 18:00:10 +0300

    add image and link split

--- src/main.py
+++ src/main.py
@@ -1,9 +1,31 @@
 from textnode import TextNode
 
 def main():
+    test_text_to_nodes()
+    #test_extract()
+    #test_images()
+
+def test_text_to_nodes():
     text = "This is **bold** text, and this is *italic* text, and finally a `code` text"
     text2 = "**BOLD** text"
-    nodes = TextNode.text_to_nodes(text2)
+    text3 = "This is **text** with an *italic* word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+    nodes = TextNode.text_to_nodes(text3)
+    for n in nodes:
+        print(n)
+
+def test_extract():
+    text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and ![another](https://i.imgur.com/dfsdkjfd.png)"
+    print(TextNode.extract_markdown_images(text))
+    # [("image", "https://i.imgur.com/zjjcJKZ.png"), ("another", "https://i.imgur.com/dfsdkjfd.png")]
+
+    text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+    print(TextNode.extract_markdown_links(text))
+    # [("link", "https://www.example.com"), ("another", "https://www.example.com/another")]
+
+def test_images():
+    text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)"
+    node = TextNode(text, "text")
+    nodes = TextNode.split_nodes_image([node])
     for n in nodes:
         print(n)
 
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+import re
+
 class TextNode:
     type_by_delimeter = {
             "**" : "bold",
@@ -21,6 +23,8 @@ class TextNode:
         nodes = TextNode.split_nodes_delimeter([node], "**", "text")
         nodes = TextNode.split_nodes_delimeter(nodes, "*", "text")
         nodes = TextNode.split_nodes_delimeter(nodes, "`", "text")
+        nodes = TextNode.split_nodes_image(nodes)
+        nodes = TextNode.split_nodes_link(nodes)
         return nodes
 
     def split_nodes_delimeter(old_nodes, delimeter, text_type):
@@ -37,4 +41,46 @@ class TextNode:
                         new_nodes.append(TextNode(n_s[i], TextNode.type_by_delimeter[delimeter]))
             else:
                 new_nodes.append(node)
+        return new_nodes
+
+    def extract_markdown_images(text):
+        return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+    def extract_markdown_links(text):
+        return re.findall(r"\[(.*?)\]\((.*?)\)", text)
+
+    def split_nodes_image(old_nodes):
+        new_nodes = []
+        for node in old_nodes:
+            images = TextNode.extract_markdown_images(node.text)
+            if len(images) == 0:
+                new_nodes.append(node)
+            else:
+                text = node.text
+                for image in images:
+                    split = text.split(f"![{image[0]}]({image[1]})", 1)
+                    if len(split[0]) > 0:
+                        new_nodes.append(TextNode(split[0], "text"))
+                    new_nodes.append(TextNode(image[0], "image", image[1]))
+                    text = split[1]
+                if len(text) > 0:
+                    new_nodes.append(TextNode(text, "text"))
+        return new_nodes
+
+    def split_nodes_link(old_nodes):
+        new_nodes = []
+        for node in old_nodes:
+            links = TextNode.extract_markdown_links(node.text)
+            if len(links) == 0:
+                new_nodes.append(node)
+            else:
+                text = node.text
+                for link in links:
+                    split = text.split(f"[{link[0]}]({link[1]})", 1)
+                    if len(split[0]) > 0:
+                        new_nodes.append(TextNode(split[0], "text"))
+                    new_nodes.append(TextNode(link[0], "link", link[1]))
+                    text = split[1]
+                if len(text) > 0:
+                    new_nodes.append(TextNode(text, "text"))
         return new_nodes
